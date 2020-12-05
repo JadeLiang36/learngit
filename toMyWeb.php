847d08b85f0d22eb8318546a425418c839c374ce
lzx <813506005@qq.com>
2020-12-05 18:47:11 +0800

    branch test

---
+++ toMyWeb.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>toMyWeb</title>
+		<style type="text/css">
+			.main{
+				margin: 150px auto 35px;
+				padding: 20px;
+				width: 533px;
+				heigh: 300px;
+				background: salmon;
+				text-align: center;
+				font-size: 250%;
+			}
+			.hint{
+				margin: auto auto;
+				height: 80px;
+				font-size: 17px;
+				display: block;
+				text-align: center;
+			}
+			.bottom{
+				padding-bottom: 25px;
+			}
+		</style>
+	</head>
+	<body>
+		<form action="EnterMyWeb.php" method="post">
+			<div id="main" class="main"> 
+				<h3>Do you want to </h3>
+				<h3>enter the Website?</h3>
+			</div>
+		 	<div class="hint">
+				<h3> Enter your name please : <input type="text" name="username"> &nbsp; <input type="submit" name="submit" value="ok">
+				</h3>
+			</div>
+		</form>
+	</body>
+</html>
